d3cdc1a867606858046c578a9fabe23c4b4ebf17
MustelaErmine <verkhozinag@gmail.com>
2025-01-20 11:52:04 +0300

    Started update
    deleted interstitial ads
    changed weight

--- Assets/Scripts/Data/LevelManager.cs
+++ Assets/Scripts/Data/LevelManager.cs
@@ -62,9 +62,9 @@ public class LevelManager : MonoBehaviour
     public static void GoToLevel(string levelName)
     {
         print(AdsHandler.instance);
-        if (UnityEngine.Random.value < 0.3f && AdsHandler.instance != null && !Save.Instance.adfree)
-            AdsHandler.instance.ShowInterstitial(levelName);
-        else
+        //if (UnityEngine.Random.value < 0.3f && AdsHandler.instance != null && !Save.Instance.adfree)
+        //    AdsHandler.instance.ShowInterstitial(levelName);
+        //else
             SceneManager.LoadScene(levelName);
     }
 }
